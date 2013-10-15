ec397901470ee74547ba22f7801b0d9b3d4514fe
Enzo Altamiranda <enzo.alt@gmail.com>
2013-10-15 18:18:21 -0300

    fixed an error in the last migration.

--- migrations/20131015204426-track-user-review-votes.js
+++ migrations/20131015204426-track-user-review-votes.js
@@ -2,13 +2,13 @@ var dbm = require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-    db.createTable(db, 'user_review_table', {
+    db.createTable('usuario_review_table', {
         id: { type: 'int', primaryKey: true, autoIncrement: true },
         user_id: { type: 'int', notNull: true },
         review_id: { type: 'int', notNull: true }
-    });
+    }, callback);
 };
 
 exports.down = function(db, callback) {
-    db.dropTable('user_review_table');
+    db.dropTable('usuario_review_table', callback);
 };
