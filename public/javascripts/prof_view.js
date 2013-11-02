86b12c1ad6b78ba8892d5b4fb3b6ead4bcbc5095
Enzo Altamiranda <enzo.alt@gmail.com>
2013-11-02 14:21:25 -0300

    Added stars and rudimentary effects to the single view.

--- public/javascripts/prof_view.js
+++ public/javascripts/prof_view.js
@@ -16,14 +16,40 @@ function prof_view($, _, Backbone, models, views, collections) {
             starHalf: 'star-half-big.png',
             starOff : 'star-off-big.png',
             starOn  : 'star-on-big.png',
+            readOnly: true,
+            score  : function() {
+                return $(this).attr('data-score');
+            }
         };
 
+
+        $('#total_score').raty(stars_settings);
+
+        $('#score_dinamica').raty(stars_settings);
+        $('#score_conocimientos').raty(stars_settings);
+        $('#score_claridad').raty(stars_settings);
+        $('#score_pasion').raty(stars_settings);
+        $('#score_compromiso').raty(stars_settings);
+        $('#score_exigencia').raty(stars_settings);
+
+        stars_settings.readOnly = false; // Because these are used to review the prof.
         $('#star_dinamica').raty(stars_settings);
         $('#star_conocimientos').raty(stars_settings);
         $('#star_claridad').raty(stars_settings);
         $('#star_pasion').raty(stars_settings);
         $('#star_compromiso').raty(stars_settings);
         $('#star_exigencia').raty(stars_settings);
+
+        $('#calificar_button').click(function() {
+            $('#review_form').removeClass('hide');
+            $('#' + this.id).addClass('hide');
+            $('#professor_profile').removeClass('bottom_border');
+        });
+        $('#cancel_review').click(function() {
+            $('#review_form').addClass('hide');
+            $('#professor_profile').addClass('bottom_border');
+            $('#calificar_button').removeClass('hide');
+        });
 
         var reviews_view = new views.ReviewContainerView({
             el          : 'div#reviews',
